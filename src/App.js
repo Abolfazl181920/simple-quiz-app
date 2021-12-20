96dd038fd9022e7ee265249e9279de60f66d1509
Abolfazl <abolfazlm181920@gmail.com>
2021-12-20 20:00:55 +0330

    add htmls

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+
 const App = () => {
+
   return (
     <div className="App">
-      <h4 className="text-blue-100">App</h4>
+        <div className="bg-indigo-600 h-screen w-full">
+          <div>What is the capital of Germany?</div>
+          <div>
+            <input type='button' value='Tehran' />
+            <input type='button' value='Tokyo' />
+            <input type='button' value='Istanbul' />
+            <input type='button' value='nothing' />
+          </div>
+        </div>
     </div>
   );
 }
