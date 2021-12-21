daf89ad25320a0a470d012a281cc67087bdfd70c
Abolfazl <abolfazlm181920@gmail.com>
2021-12-21 16:33:03 +0330

    add new questions to data

--- src/App.js
+++ src/App.js
@@ -22,15 +22,50 @@ const App = () => {
 				{ answerText: 'Tony Stark', isCorrect: false },
 			],
 		},
+		{
+			questionText: 'Who is CEO of Apple?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Steve Jobs', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'What is the capital of Iran?',
+			answerOptions: [
+				{ answerText: 'Munich', isCorrect: false },
+				{ answerText: 'Tehran', isCorrect: true },
+				{ answerText: 'Baqdad', isCorrect: false },
+				{ answerText: 'Mazandaran', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is the best soccer player of portugal?',
+			answerOptions: [
+				{ answerText: 'Lionel Messi', isCorrect: false },
+				{ answerText: 'Cristiano Ronaldo', isCorrect: true },
+				{ answerText: 'Bernardo silva', isCorrect: false },
+				{ answerText: 'Harry maguire', isCorrect: false },
+			],
+		},
 	];
 
 	const [showScore, setShowScore] = useState(false);
 	const [score, setScore] = useState(0);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
 
 	const handleOptionClick = (isCorrect) => {
 		if (isCorrect) {
 			setScore(score + 1);
 			alert(score);
+		}
+
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowScore(true);
 		}
 	}
 
