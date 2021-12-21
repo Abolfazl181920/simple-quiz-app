4243a99905f70e8630818277f2464e1ddce76438
Abolfazl <abolfazlm181920@gmail.com>
2021-12-21 19:00:42 +0330

    create InfoBody component

--- src/Info.js
+++ src/Info.js
@@ -1,4 +1,5 @@
 import React, {Fragment} from 'react';
+import InfoBody from './InfoBody';
 
 const Info = () => {
 
---
+++ src/InfoBody.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+
+const InfoBody = () => {
+    return(
+        <Fragment>
+            <div>
+                <div className="">
+                </div>
+            </div>
+        </Fragment>
+    );
+}
+
+export default InfoBody;
