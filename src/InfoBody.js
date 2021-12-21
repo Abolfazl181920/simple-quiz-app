8fb7df5d40d19e904838eda88785c690e70d8a48
Abolfazl <abolfazlm181920@gmail.com>
2021-12-21 19:21:47 +0330

    add static of InfoBody component

--- src/InfoBody.js
+++ src/InfoBody.js
@@ -4,7 +4,11 @@ const InfoBody = () => {
     return(
         <Fragment>
             <div>
-                <div className="InfoBody"></div>
+                <div className="InfoBody">
+                    <div>You answered n questions</div>
+                    <div>You can't answer to n questions</div>
+                    <div>Your percentage is n%</div>
+                </div>
             </div>
         </Fragment>
     );
